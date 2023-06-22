b9b5dadb3f3ae55fae1bb660b58399b6a845e5ca
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-06-22 22:38:17 +0530

    header changes

--- config/site.ts
+++ config/site.ts
@@ -5,10 +5,10 @@ export const siteConfig = {
   description:
     "Get answers to your daily life concerns with Vedic Wisdom",
   mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
+    // {
+    //   title: "Home",
+    //   href: "/",
+    // },
   ],
   links: {
     twitter: "https://twitter.com/shadcn",
